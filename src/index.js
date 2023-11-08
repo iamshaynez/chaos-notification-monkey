7338dba1e854435bf3cebabce3e3201eff133265
Xiaowen.Z <xiaowen.z@outlook.com>
2023-11-08 21:30:47 +0800

    change: add random method logic

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import { sendMessage } from "./telegram.js";
-import { messageProgressBar } from './progress.js';
+import { messageProgressBar } from "./progress.js";
 import { initEnv } from "./env.js";
-import { assistantMessage } from './utils.js';
+import { assistantMessage } from "./utils.js";
+
+const messageMap = {
+    messageProgressBar: messageProgressBar,
+};
+
+function callRandomMethod(obj) {
+    // 获取对象的所有键（方法名），并存入数组
+    const keys = Object.keys(obj);
+    // 随机选择一个键
+    const randomKey = keys[Math.floor(Math.random() * keys.length)];
+    // 调用选中的方法
+    return obj[randomKey]();
+}
 
 function randomWithProbability(threshold) {
     return Math.random() < threshold ? 1 : 0;
@@ -10,15 +23,16 @@ function randomWithProbability(threshold) {
 export default {
     async fetch(request, env) {
         initEnv(env);
-        console.log(`received request...`)
+        console.log(`received request...`);
 
-        if(randomWithProbability(0.1) === 0){
+        if (randomWithProbability(1) === 0) {
             return new Response("Nothing happened!", { status: 200 });
         }
 
-        let message = messageProgressBar();
+        let message = callRandomMethod(messageMap);
 
-        await sendMessage(assistantMessage(message, `混沌消息助手`))
+        console.log(`message: ${message}`);
+        //await sendMessage(assistantMessage(message, `混沌消息助手`));
         return new Response("Success!", { status: 200 });
     },
 };
